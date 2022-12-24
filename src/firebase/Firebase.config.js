46f98eb3d1ba863b8b04a2dd78d7fb1ed61009fd
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-12-24 19:32:05 +0600

    initial commit

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,12 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDNPdItoZTOsxruCcCvbt3lbkGbC7zABk0",
+  authDomain: "simple-firebase-authenti-28556.firebaseapp.com",
+  projectId: "simple-firebase-authenti-28556",
+  storageBucket: "simple-firebase-authenti-28556.appspot.com",
+  messagingSenderId: "70619913878",
+  appId: "1:70619913878:web:72dbc4701152c27fab2cb7"
+};
+
+const app = initializeApp(firebaseConfig);
